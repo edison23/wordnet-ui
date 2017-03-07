819ee81bd78c0b5f15f8308960148e453143806d
edison23 <klementdavid@gmail.com>
2017-03-07 11:35:13 +0100

    Implemented basic HTML population from JSON

--- populateHTML.js
+++ populateHTML.js
@@ -7,16 +7,44 @@ function getData(coalback) {
 	      xhr.overrideMimeType("application/json");
 	    }
 	  },
-	  dataType: 'json',
-	  data: data,
-	  success: function(data) {
-	  	console.log(data);
-	  }
+	dataType: 'json',
+	success: coalback,
+	error: function() {
+		console.log("Way hay, what shall we do with the drunken sailor?")
+	},
+	complete: function(e, xhr, settings) {
+		console.log("status code: ", e.status )
+	},
 	});
 }
 
-// pak nekde zavolam tu getData() a dam ji call na funkci populateHTML asi nejak
-function populateHTML(data) {
-	// data = getData()
-	console.log(data);
+getData(populateHTML.bind(null));
+
+function populateHTML(word) {
+	// console.log(word);
+	$("#wordPOS").html(word.pos);
+	$("#wordID").html(word.id);
+	$("#wordMain").html(word.synset[0].name);
+	$("#wordDef").html(word.def);
+
+	
+	$.each(word.paths, function(i, path) {
+		$("#paths").append('<div class="btn-group btn-breadcrumb" id="breadcrumbs-' + i + '">')
+		$.each(path.breadcrumbs, function(j, breadcrumb) {
+			$("#breadcrumbs-" + i).append('<a href="#" class="btn btn-default">' + breadcrumb.name + '</a>');
+		})
+	})
+
+	$.each(word.children, function(i, relations) {
+		if (relations.name !== "hyperCat") {
+			$("#semGroups").append('<div class="sem-rels col-lg-4 col-md-6 col-xs-12" id="semGroup-' + i + '">\n\
+			                 <ul class="list-group" id="list-col-' + i + '">\n\
+			                 <li class="list-group-item head" id="semGroups-head-' + i + '">\
+			                 ' + relations.name + '</li>'
+			                 );
+			$.each(relations.children, function(j, synsets) {
+				$('#list-col-' + i).append('<li class="list-group-item">' + synsets.synset[0].name + '</li>');
+			});
+		};
+	});
 }
